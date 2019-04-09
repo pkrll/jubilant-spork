83be4e552550854dcd33088503a7b2941b759774
Ardalan Samimi <ardalan.samimi@gmail.com>
2019-04-09 21:19:31 +0200

    Added webhook

--- config/index.js
+++ config/index.js
@@ -1,4 +1,7 @@
 module.exports = {
 	remoteURL: "https://raw.githubusercontent.com/pkrll/furry-potato/master/",
-	downloadPath: "assets/"
+	rootDir: "caches/",
+	docDir: "posts/",
+	imgDir: "images/",
+	localRepo: "/home/veganvin/new.saturnfive.se/furry-potato"
 };
---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+const creds  = require('./config/credentials.json');
+const http   = require('http');
+const crypto = require('crypto');
+const exec = require('child_process').exec;
+const path = require('./config').localRepo;
+
+http.createServer((req, res) => {
+	req.on("data", chunk => {
+		exec('rm -f success.txt fail.txt');
+
+		const signature = "sha1=" + crypto.createHmac('sha1', creds.secret).update(chunk.toString()).digest('hex');
+
+		if (req.headers['x-hub-signature'] == signature) {
+			exec('cd ' + path + ' && git pull');
+		} else {
+			exec('touch fail.txt');
+		}
+	});
+
+	res.end("Request received");
+}).listen(3000);
