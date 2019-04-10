c45f7d6a836b865b5ef1a8ea6e83ad0ba94942ce
Ardalan Samimi <ardalan.samimi@gmail.com>
2019-04-10 16:24:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,19 +5,38 @@ const crypto = require('crypto');
 const exec = require('child_process').exec;
 const path = require('./config').localRepo;
 
+const buildSignature = (body) => {
+	const signature = crypto.createHmac('sha1', creds.secret)
+	                      .update(body.toString())
+												.digest('hex');
+
+	return "sha1=" + signature;
+};
+
+const verifySignature = (signature, body) => {
+	const standard = buildSignature(body);
+	return signature == standard;
+};
+
 http.createServer((req, res) => {
-	req.on("data", chunk => {
-		exec('rm -f success.txt fail.txt');
+	let body = "";
 
-		const signature = "sha1=" + crypto.createHmac('sha1', creds.secret).update(chunk.toString()).digest('hex');
+	req.on("data", chunk => body += chunk);
 
-		if (req.headers['x-hub-signature'] == signature) {
+	req.on("end", () => {
+		const signature = req.headers['x-hub-signature'];
+
+		let message = "Authentication failed.";
+		let status  = 404;
+
+		if (verifySignature(signature, body)) {
 			exec('cd ' + path + ' && git pull');
-			exec('touch success.txt');
-		} else {
-			exec('touch fail.txt');
+			message = "Request received!";
+			status  = 200;
 		}
-	});
 
-	res.end("Request received");
+		res.writeHead(status, { "Content-Type": "text/plain" });
+		res.write(message);
+		res.end();
+	});
 }).listen(3000);
